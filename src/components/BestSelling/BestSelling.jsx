21dc06f9fdae3673e505acd97711ac00ea1d2d51
Moaz Karem <Moazkarem@gmail.com>
2024-07-17 00:18:41 +0300

    end best selling component design

--- src/components/BestSelling/BestSelling.jsx
+++ src/components/BestSelling/BestSelling.jsx
@@ -15,15 +15,26 @@ const BestSelling = () => {
       </div>
       <div className="w-full max-w-screen-lg grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center">
         {images.slice(0, 4).map((image, index) => (
-          <div
-            key={index}
-            className="w-[240px] h-[288px] flex justify-center items-center overflow-hidden"
-          >
-            <img
-              src={image}
-              alt={`Best Selling ${index}`}
-              className="rounded-lg w-full h-full object-cover"
-            />
+          <div key={index} className="relative selling-cont">
+            <div className="w-[240px] h-[288px] flex justify-center items-center overflow-hidden">
+              <img
+                src={image}
+                alt={`Best Selling ${index}`}
+                className="rounded-lg w-full h-full object-cover"
+                loading="lazy"
+              />
+            </div>
+            <div className=" absolute left-4 -bottom-10  best-details w-52 p-3 bg-white shadow-2xl rounded-md">
+              <span className=" text-[#133a5e] text-[16px]">yellow chair</span>
+              <div className="pt-1 flex justify-between items-center">
+                <span className="bg-[#8fc83d] px-3 rounded-md text-white">o</span>
+                <div className="flex flex-col  items-center">
+                <span className="text-black font-bold "> $100</span>
+                <span className="text-black line-through  "> $100</span>
+
+                </div>
+              </div>
+            </div>
           </div>
         ))}
       </div>
